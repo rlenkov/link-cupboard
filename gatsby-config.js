783acb6c85029d7128813f39f270f88a5f4d5bef
Richard Lenkovits <richard.lenkovits@gmail.com>
2021-06-28 19:14:34 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Link Cupboard",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
